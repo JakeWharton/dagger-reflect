7b1db8089d61d9088d92a5c4248dc07f5fa8cfe4
Jake Wharton <jakew@google.com>
2019-02-04 14:45:55 -0500

    Add tests for @Binds into multibinding which is not yet supported by reflect

---
+++ integration-tests/src/main/java/com/example/BindsIntoSet.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import dagger.Binds;
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoSet;
+import java.util.Set;
+
+@Component(modules = BindsIntoSet.Module1.class)
+interface BindsIntoSet {
+  Set<String> strings();
+
+  @Module
+  abstract class Module1 {
+    @Provides static String string() {
+      return "foo";
+    }
+
+    @Binds
+    @IntoSet
+    abstract String setString(String foo);
+  }
+}
