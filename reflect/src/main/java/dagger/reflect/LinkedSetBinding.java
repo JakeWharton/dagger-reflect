37e1005152700cafb5b85b738f62ae6fa964dd4a
Jake Wharton <jakew@google.com>
2019-02-15 00:29:00 -0500

    Add @ElementsIntoSet support

--- reflect/src/main/java/dagger/reflect/LinkedSetBinding.java
+++ reflect/src/main/java/dagger/reflect/LinkedSetBinding.java
@@ -6,15 +6,21 @@ import java.util.Set;
 
 final class LinkedSetBinding<T> extends Binding.LinkedBinding<Set<T>> {
   private final List<LinkedBinding<T>> elementBindings;
+  private final List<LinkedBinding<Set<T>>> elementsBindings;
 
-  LinkedSetBinding(List<LinkedBinding<T>> elementBindings) {
+  LinkedSetBinding(List<LinkedBinding<T>> elementBindings,
+      List<LinkedBinding<Set<T>>> elementsBindings) {
     this.elementBindings = elementBindings;
+    this.elementsBindings = elementsBindings;
   }
 
   @Override public Set<T> get() {
-    Set<T> elements = new LinkedHashSet<>(elementBindings.size());
+    Set<T> elements = new LinkedHashSet<>();
     for (LinkedBinding<T> elementBinding : elementBindings) {
       elements.add(elementBinding.get());
+    }
+    for (LinkedBinding<Set<T>> elementsBinding : elementsBindings) {
+      elements.addAll(elementsBinding.get());
     }
     return elements;
   }
