91c3be0ac2ca8cd776c10e9326ce376ff23793f8
Jake Wharton <jakew@google.com>
2019-02-17 02:18:53 -0500

    Add integration test for Android
    Right now this doesn't do much, but it will host more in the future.

---
+++ integration-tests/android/src/main/java/com/example/ExampleActivity.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+import dagger.Module;
+import dagger.android.AndroidInjection;
+import dagger.android.ContributesAndroidInjector;
+import javax.inject.Inject;
+
+import static android.util.TypedValue.COMPLEX_UNIT_DIP;
+import static android.view.Gravity.CENTER;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+
+public final class ExampleActivity extends Activity {
+  @Inject String string;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    AndroidInjection.inject(this);
+    super.onCreate(savedInstanceState);
+
+    TextView textView = new TextView(this);
+    textView.setLayoutParams(new LayoutParams(MATCH_PARENT, MATCH_PARENT));
+    textView.setGravity(CENTER);
+    textView.setTextSize(COMPLEX_UNIT_DIP, 40);
+    textView.setText(string);
+    setContentView(textView);
+  }
+
+  @Module
+  static abstract class ExampleActivityModule {
+    @ContributesAndroidInjector
+    abstract ExampleActivity activity();
+  }
+}
