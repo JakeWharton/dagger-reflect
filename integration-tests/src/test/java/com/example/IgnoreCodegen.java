f944b062d20ebe64abc13245cd35076399d037ca
Jake Wharton <jakew@google.com>
2019-07-01 21:31:32 -0400

    Use annotations instead of methods to ignore integration tests
    This lets us actually validate that expected-failing reflection tests are actually failing.

---
+++ integration-tests/src/test/java/com/example/IgnoreCodegen.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(METHOD)
+@Retention(RUNTIME)
+@interface IgnoreCodegen {}
